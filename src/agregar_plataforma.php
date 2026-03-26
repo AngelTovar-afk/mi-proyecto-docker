84b50519e78fad6a52f00a8da50ecf3e5807521b
Isis23it <isachico2364@gmail.com>
2026-03-25 19:50:42 -0600

    feat: plataformas y juego de plataforma

--- src/agregar_plataforma.php
+++ src/agregar_plataforma.php
@@ -3,11 +3,11 @@ require 'conexion.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt = $pdo->prepare(
-        'INSERT INTO juego_plataforma (juego_id, plataforma_id) VALUES (?, ?)'
+        'INSERT INTO plataformas (nombre, fabricante) VALUES (?, ?)'
     );
     $stmt->execute([
-        $_POST['juego_id'],
-        $_POST['plataforma_id']
+        $_POST['nombre'],
+        $_POST['fabricante']
     ]);
     header('Location: index.php');
     exit;
